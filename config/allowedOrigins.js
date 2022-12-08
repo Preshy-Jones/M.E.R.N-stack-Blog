7e2e7335807e42ba47f3b746c43664a6e330e7eb
Precious Adedibu <adedibuprecious@gmail.com>
2022-12-08 08:04:14 +0100

    updated cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -8,6 +8,8 @@ const allowedOrigins = [
   "http://127.0.0.1:3001",
   "http://localhost:3002",
   "http://127.0.0.1:3002",
+  "http://127.0.0.1:5173",
+  "http://localhost:5173",
 ];
 
 module.exports = allowedOrigins;
